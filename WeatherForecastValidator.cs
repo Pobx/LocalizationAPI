ce74dfb1c4694d4f7bc6119584bb88c6a85b1265
Pobx <geidtiphong.s@allrightbox.com>
2022-05-31 09:58:43 +0700

    create new model class

---
+++ WeatherForecastValidator.cs
@@ -0,0 +1,19 @@
+﻿using FluentValidation;
+using Microsoft.Extensions.Localization;
+
+namespace LocalizationAPI
+{
+    public class WeatherForecastValidator : AbstractValidator<TestWeatherForcast>
+    {
+
+        public WeatherForecastValidator(IStringLocalizer<TestWeatherForcast> localizer)
+        {
+            RuleFor(x => x.Name).NotNull().WithMessage(x => localizer["HELLO"]);
+        }
+    }
+
+    public class TestWeatherForcast
+    {
+        public string Name { get; set; }
+    }
+}
